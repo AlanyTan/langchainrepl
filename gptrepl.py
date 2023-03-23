45b5f594115f01ea05b67eed85aef4bdf09dfc10
Alan Tan <atan@acerta.ai>
2023-03-23 14:23:45 +0000

    devcontainer

--- gptrepl.py
+++ gptrepl.py
@@ -9,11 +9,11 @@ load_dotenv()
 from langchain.agents import load_tools
 from langchain.agents import initialize_agent
 from langchain.agents.tools import Tool
-from langchain.agents.react.base import DocstoreExplorer
-from langchain.docstore.wikipedia import Wikipedia
 from langchain.llms.openai import OpenAI
 from langchain.tools.python.tool import PythonREPLTool
-from langchain.utilities import GoogleSearchAPIWrapper
+#from langchain.utilities import GoogleSearchAPIWrapper
+#from langchain.docstore.wikipedia import Wikipedia
+#from langchain.agents.react.base import DocstoreExplorer
 
 from googleapiclient.discovery import build
 
@@ -49,29 +49,17 @@ def stack_exchange (search_term):
 def manual_input(prompt): 
     return input(f"\n ==> Dear Human: {prompt} ")
 
-tools = load_tools(["google-search",])
+tools = load_tools(["google-search","wikipedia"])
 tools.append ( PythonREPLTool(name="Python REPL", description="Execute Python code") )
 tools.append ( Tool("ask the human", manual_input, "A tool that prompts the user for input.") )
-# docstore=DocstoreExplorer(Wikipedia())
-# tools.append ( Tool(
-#     name="Lookup Wikipedia",
-#     func=docstore.search,
-#     description="Search for a concept in the wikipedia site.",
-#   ))
-# tools.append (
-#   Tool(
-#     name="Retrieve a Wikipedia article",
-#     func=docstore.lookup,
-#     description="Retrieve a document from the wikipedia the docstore.",
-#   ))
 tools.append ( Tool("search Stack Overflow", stack_exchange, "A tool that looks up stack overflow for potential answers with samples.") )
 agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True) # type: ignore
 
 #question = "look up for SPC study in manufacturing, and write a python code that calculates cpk of a series of 20 numbers that were randomly generated"
 #question = "Learn how to calculate sigma using C4 constant in SPC study in manufacturing, which is different than numpy standard deviation, then write python code to calcuate sigma of a set of 30 random numbers."
-question = "lookup how to use redis and write a python code that subscribe to redis stream to wait for incoming data and calculate standard deviation once receive 20 numbers."
+#question = "lookup how to use redis and write a python code that subscribe to redis stream to wait for incoming data and calculate standard deviation once receive 20 numbers."
 #question = "lookup how to use redis and write a python code that publish 40 random numbers to redis stream <rstream>."
 #question = "search google what is an openfeature provider, and write a LaunchDarkly python-client-sdk based openfeature provider in python"
-#question = "Research how to calculate SPC study in manufacturing and write python code to calculate cp, cpk, and pp, ppk using D2 constant"
+question = "Research how to calculate SPC study in manufacturing using AIAG guidelines, and write python code to calculate cp, cpk, and pp, ppk using D2 constant"
 agent.run(question)
 
