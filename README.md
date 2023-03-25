4ba3ec57eaba1cf7641dd8b614c47aa58de76492
Alan Tan <atan@acerta.ai>
2023-03-25 04:20:20 +0000

    added wolfram-alpha as a source and redis cache

--- README.md
+++ README.md
@@ -1,10 +1,75 @@
 # langchainrepl
 LangChain plus Repl and GPT
 
+Initial experiment with GPT and Langchain with tools that allow LLM to code, debug using REPL, and lookup Google, Stack Overflow, Wikipedia and ask human for help to learn and improve its coding work. 
+
+
 ## setup
 Please create a .env file (or set up environment variables as below) with the content:
 ```
+OPENAI_APIT_KEY=${the OpenAI api key}
 GOOGLE_API_KEY=${the Google Custom Search Engine API key}
 GOOGLE_CSE_ID=${the Google Custom Search Engine ID}
-OPENAI_APIT_KEY=${the OpenAI api key}
+STACKEXCHANGE_API_KEY=${stackexchange_api_key}
+WOLFRAM_ALPHA_APPID=${wolfram-alpha appid}
+REDIS_HOST=localhost
+REDIS_PORT=6379
+```
+The only mandatory required value is the OPENAI_API_KEY, all the others are optional. 
+
+An example output: it searches for Stack Overflwo first, then tries its Python code using REPL, found an error, tried Stack Overflow to solve that error, then found another error, and asked human to help. 
+Human imput was in natural language, it was able to fix the bug with this hint. 
+
+```
+> Entering new AgentExecutor chain...
+ I need to find out how to use redis and write a python code
+Action: search Stack Overflow
+Action Input: "how to use redis and write a python code"
+Observation: Can I use Redis installed on Windows Subsystem for Linux with a python app in Windows?: "Yes, you can use redis from wsl from windows. First, make sure you've installed and started the redis service:\n\nsudo apt-get install redis-server\nsudo systemctl enable redis-server.service\n\n\nif you already run a redis server on windows, you'll need to edit the port directive in /etc/redis/redis.conf (e.g. to 7379 like I've done for the commands below).\n\nThen start the service\n\nsudo service redis-server start\n\n\nand then run redis-cli and issue the monitor command (you can skip the -p 7379 if you're using the default port):\n\nbp@bjorn8:~$ redis-cli -p 7379\n127.0.0.1:6379> monitor\nOK\n\n\nnow, from your windows command prompt, install the redis module from pypi (https://pypi.org/project/redis/):\n\npip install redis\n\n\nthen start python and issue a test command (again, use 6379 if you're using the default port):\n\n>>> import redis\n>>> cn = redis.Redis('localhost', 7379, 0)\n>>> cn.keys(\"*\")\n[]\n\n\nin your wsl session you should now see something like:\n\n1558196107.718695 \"KEYS\" \"*\"\n\n\nNote: redis is not fuzzy about where the server is. If you have the cli tools installed on windows you can issue commands from dos to the server running on wsl:\n\nc:\\srv> redis-cli -p 7379\nredis 127.0.0.1:7379> keys \"*\"\n(empty list or set)\nredis 127.0.0.1:7379>\n\n\nand vice-versa (redis-cli on wsl will happily connect to a redis service running on windows -- which is how I discovered I needed to specify different ports ;-)\n".
+
+Thought: I now know how to use redis and write a python code
+Action: Python REPL
+Action Input: import redis
+Observation: 
+Thought: I now know how to use redis and write a python code to emit 40 random numbers to redis stream <rstream>
+Action: Python REPL
+Action Input: import random
+Observation: 
+Thought: I now know how to use redis and write a python code to emit 40 random numbers to redis stream <rstream>
+Action: Python REPL
+Action Input: for i in range(40):
+Observation: expected an indented block after 'for' statement on line 1 (<string>, line 1)
+Thought: I need to find out how to fix the error
+Action: search Stack Overflow
+Action Input: "expected an indented block after 'for' statement on line 1"
+Observation: basic Python if statement throw error IndentationError: "You may receive an "IndentationError" in Python when your code is not indented correctly. In your case, however, you do not have an indented block after your if statement on line 1.\n"."That is because you aren't indenting property in cmd.\n>>> a = 1\n>>> if a == 1:\n...     print("a == 1")\na == 1\n\nTo indent in cmd, simply press tab, and it will indent.\n".
+
+Thought: I now know how to fix the error
+Action: Python REPL
+Action Input: for i in range(40):
+    rstream.xadd('mystream', {'random_number': random.randint(1,100)})
+Observation: name 'rstream' is not defined
+Thought: I need to find out how to define rstream
+Action: search Stack Overflow
+Action Input: "how to define rstream"
+Observation: Can not find an answer
+
+Thought: I need to ask a human for help
+Action: ask the human
+Action Input: "How do I define rstream in Python?"
+ ==> Dear Human: How do I define rstream in Python? :before you use rstream to perform xadd, you need to define it as a redis stream first
+
+Observation: before you use rstream to perform xadd, you need to define it as a redis stream first
+Thought: I now know how to define rstream
+Action: Python REPL
+Action Input: rstream = redis.Redis('localhost', 7379, 0)
+Observation: 
+Thought: I now know how to use redis and write a python code to emit 40 random numbers to redis stream <rstream>
+Final Answer: To emit 40 random numbers to redis stream <rstream>, you need to define rstream as a redis stream first, then use a for loop to generate 40 random numbers and use the xadd command to add them to the stream. The code should look like this:
+
+rstream = redis.Redis('localhost', 7379, 0)
+for i in range(40):
+    rstream.xadd('mystream', {'random_number': random.randint(1,100)})
+
+> Finished chain.
 ```
--- gptrepl.py
+++ gptrepl.py
@@ -1,65 +1,125 @@
 #from typing import Dict, Optional
 import os
 import re
-import dotenv
+import json
+#import dotenv
+import redis
 import requests
 import html
-from dotenv import load_dotenv
-load_dotenv()
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 from langchain.agents import load_tools
 from langchain.agents import initialize_agent
 from langchain.agents.tools import Tool
 from langchain.llms.openai import OpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain.tools.python.tool import PythonREPLTool
+from langchain.memory import ConversationBufferMemory
+from langchain.utilities.wolfram_alpha import WolframAlphaAPIWrapper
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 #from langchain.utilities import GoogleSearchAPIWrapper
 #from langchain.docstore.wikipedia import Wikipedia
 #from langchain.agents.react.base import DocstoreExplorer
 
 from googleapiclient.discovery import build
 
-llm = OpenAI(model_name="text-davinci-003",temperature=0.00) # type: ignore
+from bs4 import BeautifulSoup
+
+if (v := os.getenv('REDIS_HOST')) is not None:
+    redis_host = v
+else:
+    redis_host = None
+if (v := os.getenv('REDIS_PORT')) is not None:
+    redis_port = int(v)
+else:
+    redis_port = 6379
+if redis_host is not None:
+    r = redis.Redis(host=redis_host, port=redis_port, db=0)
+current_stackexchange_count=0
+
+llm = OpenAI(model_name="text-davinci-003",temperature=0.00,max_retries=10) # type: ignore
+
+def retrieve_webpage (input):
+    url_pattern = re.compile(r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))")
+
+    match = re.findall(url_pattern,input)
+    print(f'match:{match}')
+    contents = ''
+    if match:
+        for url in match: # type: ignore
+            print (f'url:{url[0]}')
+            response = requests.get(url[0])
+            if response.status_code == 200:
+                webpage_content = BeautifulSoup(response.content, 'html.parser')
+                contents = contents + " " + webpage_content.get_text()
+            else:
+                contents = contents + f'the url {url} cannot be retrieved. '
+            return contents[0:2000]
+    else :
+        return 'you did not provide a url in your ask, I can only give you an answer if you give me a url.'
 
 def remove_html_tags(text):
     clean = re.compile('<.*?>')
     return html.unescape(re.sub(clean, '', text))
 
 def stack_exchange (search_term):
+    global current_stackexchange_count
     site = 'stackoverflow'
-    api_key = os.getenv("STACKEXCHANGE_API_KEY")
+    if (v := os.getenv('STACKEXCHANGE_API_KEY')) is not None:
+        api_key_parm = f'&key={v}'
+    else: 
+        api_key_parm = r''
     base_url = 'https://api.stackexchange.com/2.3'
-    search_url = f'{base_url}/search/advanced?order=desc&tagged=python&sort=relevance&site={site}&key={api_key}&q={search_term}'
+    try :
+        search_json = json.loads(r.get(search_term) ) # type: ignore
+    except: 
+        search_json = None
+    if search_json is None:
+        search_url = f'{base_url}/search/advanced?answers=1&order=desc&tagged=python&sort=relevance&site={site}{api_key_parm}&q={search_term}'
+        search_response = requests.get(search_url)
+        search_json = search_response.json()
+        r.set(search_term, json.dumps(search_json))
+    if len(search_json['items'] or []) > current_stackexchange_count: 
+        question_id = search_json['items'][current_stackexchange_count]['question_id']
 
-    search_response = requests.get(search_url)
-    search_json = search_response.json()
-    if len(search_json['items']) > 0: 
-        question_id = search_json['items'][0]['question_id']
+        question_title = search_json['items'][current_stackexchange_count]['title']
 
-        url = f'{base_url}/questions/{question_id}/answers?order=desc&sort=activity&site={site}&filter=withbody&key={api_key}'
-
+        url = f'{base_url}/questions/{question_id}/answers?order=desc&sort=activity&site={site}&filter=withbody{api_key_parm}'
         response = requests.get(url)
         json_data = response.json()
-
+        current_stackexchange_count = current_stackexchange_count + 1   
+ 
         # Get the body of the first answer
-        answer_body = json_data['items'][0]['body']
+        answer_body = "I can not find an answer for your question."
+        answer_item_bodys = [json.dumps(sub_item['body']) for sub_item in json_data['items']]
+        if len(answer_item_bodys) > 0:
+            answer_body = ".".join(answer_item_bodys) + "."
 
-        return remove_html_tags(answer_body) + '\n'
+
+        return remove_html_tags(question_title + ": " + answer_body) + '\n'
     else :
         return 'Can not find an answer\n'
 
 def manual_input(prompt): 
-    return input(f"\n ==> Dear Human: {prompt} ")
+     return input(f"\n ==> Dear Human: {prompt} :")
 
-tools = load_tools(["google-search","wikipedia"])
-tools.append ( PythonREPLTool(name="Python REPL", description="Execute Python code") )
-tools.append ( Tool("ask the human", manual_input, "A tool that prompts the user for input.") )
-tools.append ( Tool("search Stack Overflow", stack_exchange, "A tool that looks up stack overflow for potential answers with samples.") )
-agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True) # type: ignore
+default_tool_list = ["google-search","wikipedia"]
+if (v := os.getenv('WOLFRAM_ALPHA_APPID')) is not None:
+    default_tool_list.append("wolfram-alpha")
+tools = load_tools(default_tool_list)
+tools.append ( PythonREPLTool(name="Python REPL", description="Used to Execute Python code") )
+tools.append ( Tool("ask the human", manual_input, "Ask human for help when search cannot find answer, or prompts the user for input.") )
+tools.append ( Tool("retrieve URL", retrieve_webpage, "https and http url used as .") ) # type: ignore
+tools.append ( Tool("search Stack Overflow", stack_exchange, "Search stack overflow for potential answers, especially if run into exceptions and errors") )
+agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True, memory=memory) # type: ignore
 
+#question = "calculate the 10th fibonacci number"
 #question = "look up for SPC study in manufacturing, and write a python code that calculates cpk of a series of 20 numbers that were randomly generated"
 #question = "Learn how to calculate sigma using C4 constant in SPC study in manufacturing, which is different than numpy standard deviation, then write python code to calcuate sigma of a set of 30 random numbers."
-#question = "lookup how to use redis and write a python code that subscribe to redis stream to wait for incoming data and calculate standard deviation once receive 20 numbers."
-#question = "lookup how to use redis and write a python code that publish 40 random numbers to redis stream <rstream>."
+#question = "write a python script that subscribe to redis stream to wait for incoming data and calculate standard deviation once receive 20 numbers."
+question = "lookup how to use redis and write a python code that emit 40 random numbers to redis stream <rstream>."
 #question = "search google what is an openfeature provider, and write a LaunchDarkly python-client-sdk based openfeature provider in python"
-question = "Research how to calculate SPC study in manufacturing using AIAG guidelines, and write python code to calculate cp, cpk, and pp, ppk using D2 constant"
+#question = "learn how to calculate SPC study in manufacturing, especially how to use D2 to calculate sigma, ppk and cpk, using AIAG guidelines. Then write python program to perform XBar analysis with a subgroup size of 5, and calculate XBar, range, cp, cpk, and pp, ppk, check with me for each calculation result."
+question = "write a python code that calculate 100 digit of PI. Make sure you can calculate 100 digits of PI, most code you found on the internet are unable to calculate that many digits"
 agent.run(question)
 
